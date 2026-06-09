c46e55e666e1b6e423e5647ba44b7c2b3a7db15f
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2026-06-09 14:20:21 +0300

    Update toolchain-arm-none-eabi.cmake

--- cmake/toolchain-arm-none-eabi.cmake
+++ cmake/toolchain-arm-none-eabi.cmake
@@ -58,7 +58,7 @@ set(MRE_SDK "$ENV{MRE_SDK}")
 # -fdata-sections       Place each data item into its own section in the output file.
 # -fomit-frame-pointer  Omit the frame pointer in functions that don’t need one.
 # -mabi=aapcs           Defines enums to be a variable sized type.
-set(OBJECT_GEN_FLAGS "-fpic -march=armv5te -fvisibility=hidden -mlittle-endian -fdata-sections -ffunction-sections -mthumb -D__MRE_COMPILER_GCC__")
+set(OBJECT_GEN_FLAGS "-fpic -march=armv5te -fvisibility=hidden -mlittle-endian -fdata-sections -ffunction-sections -D__MRE_COMPILER_GCC__")
 
 set(CMAKE_C_FLAGS   "${OBJECT_GEN_FLAGS} -std=gnu99  " CACHE INTERNAL "C Compiler options")
 set(CMAKE_CXX_FLAGS "${OBJECT_GEN_FLAGS} -std=c++11 -fno-threadsafe-statics -fno-exceptions -fno-non-call-exceptions -fno-use-cxa-atexit -fno-threadsafe-statics " CACHE INTERNAL "C++ Compiler options")
